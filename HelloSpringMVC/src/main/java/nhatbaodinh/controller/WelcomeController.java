1dbe9cd45a6662ef54db39f87729886977331374
Nhat Bao Dinh <nhatbaodinh@Baos-MacBook.local>
2025-03-03 10:28:41 +0700

    update dependencies

--- HelloSpringMVC/src/main/java/nhatbaodinh/controller/WelcomeController.java
+++ HelloSpringMVC/src/main/java/nhatbaodinh/controller/WelcomeController.java
@@ -2,11 +2,14 @@ package nhatbaodinh.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class WelcomeController {
-    @RequestMapping("/welcome")
-    public String welcome() {
-        return "welcome";
+    @RequestMapping("/hello")
+    public ModelAndView getPage(){
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("welcome");
+        return mv;
     }
 }
