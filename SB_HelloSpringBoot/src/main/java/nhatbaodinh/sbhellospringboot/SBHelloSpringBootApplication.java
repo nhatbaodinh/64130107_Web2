41f51f1e1e6b1b995f2d78871280048ccc239d4d
Nhat Bao Dinh <nhatbaodinh@192.168.1.5>
2025-03-04 21:38:59 +0700

    rename to SB_HelloSpringBoot

--- SB_HelloSpringBoot/src/main/java/nhatbaodinh/sbhellospringboot/SBHelloSpringBootApplication.java
+++ SB_HelloSpringBoot/src/main/java/nhatbaodinh/sbhellospringboot/SBHelloSpringBootApplication.java
@@ -1,13 +1,13 @@
-package nhatbaodinh.chatspringboot;
+package nhatbaodinh.sbhellospringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = "controllers")
-public class ChatSpringBootApplication {
+public class SBHelloSpringBootApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(ChatSpringBootApplication.class, args);
+    SpringApplication.run(SBHelloSpringBootApplication.class, args);
   }
 
 }
