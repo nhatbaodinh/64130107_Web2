7d6c3283b4dfb023dac034673dff569f2d597eca
Nhat Bao Dinh <nhatbaodinh@Baos-MacBook.local>
2025-03-25 09:45:42 +0700

    TongHopGK - push lan 4 - update link navbar

--- TongHopGK/src/main/java/nhatbaodinh/tonghopgk/controllers/TongHopGKController.java
+++ TongHopGK/src/main/java/nhatbaodinh/tonghopgk/controllers/TongHopGKController.java
@@ -9,4 +9,19 @@ public class TongHopGKController {
   public String index() {
     return "index";
   }
+
+  @GetMapping("/about")
+  public String about() {
+    return "about";
+  }
+
+  @GetMapping("/list")
+  public String list() {
+    return "list";
+  }
+
+  @GetMapping("/news")
+  public String news() {
+    return "news";
+  }
 }
