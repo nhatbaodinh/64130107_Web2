4e7e6104b70766da54286db773a4ed7010cdd657
Nhat Bao Dinh <nhatbaodinh@Baos-MacBook.local>
2025-04-01 09:02:36 +0700

    push lan 1, tao du an

---
+++ dinhnhatbao_ThiGiuaKy/src/main/java/thiGK/ntu64130107/DinhnhatbaoThiGiuaKyApplication.java
@@ -0,0 +1,13 @@
+package thiGK.ntu64130107;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DinhnhatbaoThiGiuaKyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DinhnhatbaoThiGiuaKyApplication.class, args);
+	}
+
+}
---
+++ dinhnhatbao_ThiGiuaKy/src/test/java/thiGK/ntu64130107/DinhnhatbaoThiGiuaKyApplicationTests.java
@@ -0,0 +1,13 @@
+package thiGK.ntu64130107;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DinhnhatbaoThiGiuaKyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
