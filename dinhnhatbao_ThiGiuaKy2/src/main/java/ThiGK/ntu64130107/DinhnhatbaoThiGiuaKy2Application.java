9f74e1cb3e38ffe5f4cd824e1b75f9217c3e72f4
Nhat Bao Dinh <nhatbaodinh@syn-172-100-155-027.res.spectrum.com>
2025-04-15 10:09:54 +0700

    Thi giua ky lam lai - Tao du an

---
+++ dinhnhatbao_ThiGiuaKy2/src/main/java/ThiGK/ntu64130107/DinhnhatbaoThiGiuaKy2Application.java
@@ -0,0 +1,13 @@
+package ThiGK.ntu64130107;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DinhnhatbaoThiGiuaKy2Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DinhnhatbaoThiGiuaKy2Application.class, args);
+  }
+
+}
---
+++ dinhnhatbao_ThiGiuaKy2/src/test/java/ThiGK/ntu64130107/DinhnhatbaoThiGiuaKy2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ThiGK.ntu64130107;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DinhnhatbaoThiGiuaKy2ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
